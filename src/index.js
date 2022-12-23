7471ffa5392cd1a9c1c963617836f0a90a6e60be
ricknobre <guilhermericardopro71@gmail.com>
2022-12-23 16:43:34 -0400

    first steps

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import { Client } from 'discord.js'
+import { Client, Partials } from 'discord.js'
+import * as dotenv from 'dotenv';
+import * as fs from 'fs'
+
+dotenv.config()
+
+const client = new Client({
+    intents: [
+        'Guilds',
+        'GuildPresences',
+        'GuildMessages',
+        'GuildVoiceStates',
+        'GuildMembers',
+        'GuildBans',
+        'MessageContent',
+        'GuildMessageReactions',
+        'AutoModerationExecution',
+        'AutoModerationConfiguration',
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.Message,
+        Partials.Reaction,
+        Partials.ThreadMember,
+        Partials.User,
+    ]
+})
+
+client.login(process.env.TOKEN)
+
+
+client.on('ready', (client) => {
+    console.log('Bot online')
+})
